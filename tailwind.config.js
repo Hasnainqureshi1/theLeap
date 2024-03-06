76465862217e645ef3442af05da39f8cf4d34b32
Hasnainqureshi1 <husnainqureshi134@gmail.com>
2024-03-07 03:16:41 +0500

    theLeap1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
 
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
+  }
+}
